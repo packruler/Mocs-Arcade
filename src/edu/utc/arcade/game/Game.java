ece1c1dba52384024b616dcb0744cf097d816cb3
packruler <packruler@gmail.com>
2016-01-20 11:29:25 -0500

    Add comments

--- src/edu/utc/arcade/game/Game.java
+++ src/edu/utc/arcade/game/Game.java
@@ -17,66 +17,155 @@ public class Game implements Comparable {
     public Game() {
     }
 
+    /**
+     * Get the title of the Game
+     *
+     * @return Title of the Game
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the title of the Game.
+     * -Should only be used for easy setup of adding a game to remote library-
+     *
+     * @param title intended title of the Game
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get the name of the Developer of the Game
+     *
+     * @return Name of the Developer of the Game
+     */
     public String getDeveloper() {
         return developer;
     }
 
+    /**
+     * Set name of Developer
+     * -Should only be used for easy setup of adding a game to remote library-
+     *
+     * @param developer intended name of Developer
+     */
     public void setDeveloper(String developer) {
         this.developer = developer;
     }
 
+    /**
+     * Get Git address of the Game repo
+     *
+     * @return Git address of the Game repo
+     */
     public String getGitAddress() {
         return gitAddress;
     }
 
+    /**
+     * Set Git address of the Game
+     * -Should only be used for easy setup of adding a game to remote library-
+     *
+     * @param gitAddress Intended Git address
+     */
     public void setGitAddress(String gitAddress) {
         this.gitAddress = gitAddress;
     }
 
+    /**
+     * Get name of the branch that should be loaded for the Game
+     *
+     * @return Name of the branch that should be used. Null if default branch should be used
+     */
     public String getGitBranch() {
         return gitBranch;
     }
 
+    /**
+     * Set the name of the Branch that should be used for this Game
+     * -Should only be used for easy setup of adding a game to remote library-
+     *
+     * @param gitBranch Intended Branch name
+     */
     public void setGitBranch(String gitBranch) {
         this.gitBranch = gitBranch;
     }
 
-    public boolean getLocal() {
+    /**
+     * Is this Game available locally
+     *
+     * @return True if Game is available locally
+     */
+    public boolean isLocal() {
         return local;
     }
 
+    /**
+     * Set whether or not Game is now available locally
+     *
+     * @param local True if Game is local
+     */
     public void setLocal(boolean local) {
         this.local = local;
     }
 
+    /**
+     * Get the localized path of Game executable
+     *
+     * @return Path of executable
+     */
     public String getExecutablePath() {
         return executablePath;
     }
 
+    /**
+     * Set the localized path of Game executable
+     * -Should only be used for easy setup of adding a game to remote library-
+     *
+     * @param executablePath Localized path to executable
+     *                       Example: <i>Repo</i>/build/executable.jar should be build/executable.jar
+     */
     public void setExecutablePath(String executablePath) {
         this.executablePath = executablePath;
     }
 
+    /**
+     * -Unsure if will be used-
+     * Will specify the format of a Game ie Java vs .exe
+     *
+     * @return
+     */
     public String getFormat() {
         return format;
     }
 
+    /**
+     * -Unsure if will be used-
+     * Will specify the format of a Game ie Java vs .exe
+     *
+     * @param format
+     */
     public void setFormat(String format) {
         this.format = format;
     }
 
-    public String[] getOperatingSystem() {
+    /**
+     * Get array of Operating Systems supported by this Game
+     *
+     * @return Array of Operating Systems supported
+     */
+    public String[] getOperatingSystems() {
         return operatingSystem;
     }
 
+    /**
+     * Set array of Operating Systems supported by this Game
+     * -Should only be used for easy setup of adding a game to remote library-
+     *
+     * @param operatingSystem Array of Operating Systems supported
+     */
     public void setOperatingSystem(String[] operatingSystem) {
         this.operatingSystem = operatingSystem;
     }
