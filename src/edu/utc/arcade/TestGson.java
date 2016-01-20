c7b88e22332d184498389bea754039451bb8edb1
packruler <packruler@gmail.com>
2016-01-20 11:55:28 -0500

    Verify OSCheck

--- src/edu/utc/arcade/TestGson.java
+++ src/edu/utc/arcade/TestGson.java
@@ -2,9 +2,11 @@ package edu.utc.arcade;
 
 import edu.utc.arcade.game.Game;
 import edu.utc.arcade.game.GameLibrary;
+import edu.utc.arcade.game.OSCheck;
 import edu.utc.arcade.logging.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Ethan Leisinger on 1/11/16.
@@ -12,9 +14,11 @@ import java.io.IOException;
 public class TestGson {
     public static void main(String[] args) {
         GameLibrary library = new GameLibrary();
+
         for (Game game : library.getLibrary()) {
             if (game.getDeveloper().equals("Packruler")) {
                 Log.i("Update: " + GitHandler.countBehind(game));
+                Log.i("Is compatible? " + OSCheck.IS_COMPATIBLE(game));
 //                Log.i("Updated? " + GitHandler.pull(game));
                 break;
             }
