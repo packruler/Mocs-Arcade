68ee669c3753e9e86e19eb242900d4e0c68cb45b
packruler <packruler@gmail.com>
2016-01-21 10:25:18 -0500

    Add Log error capability

--- src/edu/utc/arcade/logging/Log.java
+++ src/edu/utc/arcade/logging/Log.java
@@ -4,7 +4,11 @@ package edu.utc.arcade.logging;
  * Created by Ethan Leisinger on 1/5/2016.
  */
 public class Log {
-    public static void i(String text) {
-        System.out.println(text);
+    public static void i(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void e(String msg) {
+        System.err.println(msg);
     }
 }
