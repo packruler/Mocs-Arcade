3b0b12a4ecbdcc0cd33753bac4a20d7054d5e07e
packruler <packruler@gmail.com>
2016-02-10 10:45:57 -0500

    Working on game list view setup

---
+++ src/edu/utc/arcade/gui/GameListView.java
@@ -0,0 +1,7 @@
+package edu.utc.arcade.gui;
+
+/**
+ * Created by Ethan Leisinger on 2/10/16.
+ */
+public class GameListView {
+}
---
+++ src/edu/utc/arcade/gui/GameListViewCell.java
@@ -0,0 +1,15 @@
+package edu.utc.arcade.gui;
+
+import edu.utc.arcade.game.Game;
+import javafx.scene.control.ListCell;
+
+/**
+ * Created by Ethan Leisinger on 2/10/16.
+ */
+public class GameListViewCell extends ListCell<Game> {
+
+    @Override
+    protected void updateItem(Game item, boolean empty) {
+        super.updateItem(item, empty);
+    }
+}
