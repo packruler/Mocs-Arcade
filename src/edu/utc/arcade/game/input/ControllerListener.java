194af2fcac626793b050ac08eaed4414c80fa466
packruler <packruler@gmail.com>
2016-02-19 12:02:14 -0500

    In theory the basis for controller input starting

---
+++ src/edu/utc/arcade/game/input/ControllerListener.java
@@ -0,0 +1,11 @@
+package edu.utc.arcade.game.input;
+
+import net.java.games.input.ControllerEvent;
+
+import javax.sound.midi.ControllerEventListener;
+
+/**
+ * Created by Ethan Leisinger on 2/19/16.
+ */
+public class ControllerListener {
+}
