27e01ac2401be5e8a42dc2cda0936aa98b44a99f
packruler <support@packruler.com>
2016-04-25 10:48:06 -0400

    Make Game ignore LaunchInfo class

--- src/edu/utc/arcade/game/Game.java
+++ src/edu/utc/arcade/game/Game.java
@@ -25,7 +25,7 @@ public class Game implements Comparable {
     private boolean local;
     private long dataUpdateTime;
 
-    private LaunchInfo launchInfo;
+    private transient LaunchInfo launchInfo;
 
     public Game() {
     }
